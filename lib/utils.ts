375d143c8e759271784314441cd41dbff0c5cbbb
jojomatik <dev@jojomatik.de>
2022-02-24 18:03:51 +0100

    feat: add meta information to `index.vue`
    Add meta/ title information to `index.vue` and a function for simple title templating.
    
    This replaces the `vue-meta` package which has been used in most of my projects before.

---
+++ lib/utils.ts
@@ -0,0 +1,3 @@
+export function getTitle(title: string) {
+  return title + " - nuxt-3-base";
+}
--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,13 @@
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import { getTitle } from "~/lib/utils";
 
-@Options({})
+@Options({
+  head() {
+    return { title: getTitle("Home") };
+  },
+})
 export default class Home extends Vue {
   name = "world";
 }
