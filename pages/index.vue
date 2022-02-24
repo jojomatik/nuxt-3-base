b195a19dacce3b11dd58a9f5d04c37013c147bbb
jojomatik <dev@jojomatik.de>
2022-02-24 18:03:38 +0100

    feat: add class-style component syntax
    Add packages for class-style component syntax and an example of a class-style component.

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Hello {{ name }}!</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class Home extends Vue {
+  name = "world";
+}
+</script>
