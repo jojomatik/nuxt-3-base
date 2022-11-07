18bb453c26e3d6d37af4524bb1abc38039d38882
jojomatik <dev@jojomatik.de>
2022-11-07 23:19:57 +0100

    build(deps-dev): bump nuxt from 3.0.0-rc.4 to 3.0.0-rc.13 (#103) (#109)
    * build(deps-dev): bump nuxt from 3.0.0-rc.4 to 3.0.0-rc.13
    
    Bumps [nuxt](https://github.com/nuxt/nuxt.js) from 3.0.0-rc.4 to 3.0.0-rc.13.
    - [Release notes](https://github.com/nuxt/nuxt.js/releases)
    - [Changelog](https://github.com/nuxt/nuxt.js/blob/dev/RELEASE_PLAN.md)
    - [Commits](https://github.com/nuxt/nuxt.js/commits)
    
    ---
    updated-dependencies:
    - dependency-name: nuxt
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * refactor: remove import statement
    
    Remove import of `defineNuxtConfig` as it is not necessary anymore since `nuxt@3.0.0-rc.10`.
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: jojomatik <dev@jojomatik.de>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-import { defineNuxtConfig } from "nuxt";
 import { resolve, dirname } from "node:path";
 import { fileURLToPath } from "url";
 import VueI18nVitePlugin from "@intlify/unplugin-vue-i18n/vite";
