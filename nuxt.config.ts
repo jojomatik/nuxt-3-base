a0c85bf0709ac0f24c1d481447b07644a75ef173
jojomatik <dev@jojomatik.de>
2022-11-09 00:13:04 +0100

    feat: generate license report
    Generate report of all third party dependencies' licenses and output them into `public/dependencies.json`. Validate license compliance along the way. Closes #7

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import { resolve, dirname } from "node:path";
 import { fileURLToPath } from "url";
 import VueI18nVitePlugin from "@intlify/unplugin-vue-i18n/vite";
+import license from "rollup-plugin-license";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
@@ -17,6 +18,36 @@ export default defineNuxtConfig({
         include: [
           resolve(dirname(fileURLToPath(import.meta.url)), "./locales/*.json"),
         ],
+      }),
+      license({
+        thirdParty: {
+          includePrivate: false,
+          allow: {
+            test: (dependency) => {
+              // Return false for unlicensed dependencies.
+              if (!dependency.license) return false;
+
+              // Special case for `@nuxt/ui-templates` which publishes design resources under `CC-BY-ND-4.0`.
+              if (
+                dependency.name === "@nuxt/ui-templates" &&
+                dependency.license === "CC-BY-ND-4.0"
+              )
+                return true;
+
+              // Allow MIT and Apache-2.0 licenses.
+              return ["MIT", "Apache-2.0"].includes(dependency.license);
+            },
+            failOnUnlicensed: true,
+            failOnViolation: true,
+          },
+          output: {
+            // Output file into public directory which is included in the build output.
+            file: "public/dependencies.json",
+            template(dependencies) {
+              return JSON.stringify(dependencies);
+            },
+          },
+        },
       }),
     ],
   },
