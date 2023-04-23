c255e1fef6a7d4781a5b7d929806048d84b2140f
jojomatik <dev@jojomatik.de>
2023-04-23 16:55:42 +0200

    feat: add `app.vue` example
    Add `app.vue` example file which contains the `v-app`.

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-title> Nuxt 3 Base </v-app-bar-title>
+    </v-app-bar>
+    <NuxtPage />
+  </v-app>
+</template>
+
+<script setup lang="ts"></script>
--- pages/user/[userId].vue
+++ pages/user/[userId].vue
@@ -1,18 +1,16 @@
 <template>
-  <v-app>
-    <v-card style="width: 80%; max-width: 400px; margin: auto">
-      <v-card-title>
-        <div class="d-flex">
-          {{ i18n.t("user.page") }}
-          <v-spacer></v-spacer>
-          <v-icon :icon="mdiAccountCircle" />
-        </div>
-      </v-card-title>
-      <v-card-text>
-        <p>{{ i18n.t("user.welcome", { user: $route.params.userId }) }}</p>
-      </v-card-text>
-    </v-card>
-  </v-app>
+  <v-card style="width: 80%; max-width: 400px; margin: auto">
+    <v-card-title>
+      <div class="d-flex">
+        {{ i18n.t("user.page") }}
+        <v-spacer></v-spacer>
+        <v-icon :icon="mdiAccountCircle" />
+      </div>
+    </v-card-title>
+    <v-card-text>
+      <p>{{ i18n.t("user.welcome", { user: $route.params.userId }) }}</p>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script setup lang="ts">
