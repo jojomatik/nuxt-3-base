8e9acf2bd0cdbb26ffb8c0cdc0be0043906940be
jojomatik <dev@jojomatik.de>
2023-04-23 17:39:56 +0200

    feat: add image example
    Add jojomatik logo as an image example and add it to app-bar.

--- app.vue
+++ app.vue
@@ -1,13 +1,23 @@
 <template>
   <v-app>
     <v-app-bar>
-      <v-app-bar-title> Nuxt 3 Base </v-app-bar-title>
+      <v-app-bar-title>
+        <div style="display: flex; align-items: center">
+          Nuxt 3 Base by
+          <a href="https://github.com/jojomatik" class="pl-1">
+            <v-img :src="jojomatikLogo" width="120"></v-img>
+          </a>
+        </div>
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
     </v-app-bar>
     <NuxtPage />
   </v-app>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import jojomatikLogo from "assets/jojomatik.png";
+</script>
 
 <style lang="scss">
 @use "assets/main";
