c585cdebf50910ed1885781c401634eb517a47b8
jojomatik <dev@jojomatik.de>
2024-02-04 11:47:12 +0100

    get locale from globals

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -9,7 +9,7 @@ import {
   type DefaultLocaleMessageSchema,
   type LocaleMessages,
 } from "vue-i18n";
-import { addons } from "@storybook/preview-api";
+import { addons, useGlobals } from "@storybook/preview-api";
 import options from "../vuetify-options";
 import { withVuetifyTheme, DEFAULT_THEME } from "./withVuetifyTheme.decorator";
 import { allModes } from "./modes";
@@ -32,8 +32,11 @@ function loadLocaleMessages(): LocaleMessages<DefaultLocaleMessageSchema> {
   return messages;
 }
 
+const [{ locale }] = useGlobals();
+
 const i18n = createI18n({
   legacy: false,
+  locale,
   messages: loadLocaleMessages(),
 });
 
