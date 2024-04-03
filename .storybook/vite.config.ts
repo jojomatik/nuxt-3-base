69faf4e812931f10a5b8214c0996aceb1212d5f0
jojomatik <dev@jojomatik.de>
2024-04-03 22:44:37 +0200

    fix: update storybook config
    Update storybook config to use `@vitejs/plugin-vue` explicitly.

--- .storybook/vite.config.ts
+++ .storybook/vite.config.ts
@@ -1,10 +1,12 @@
 import path from "path";
 import { defineConfig } from "vite";
 import AutoImport from "unplugin-auto-import/vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    vue(),
     AutoImport({
       imports: ["vue", "vue-i18n", "vue-router"],
       dirs: ["./composables"],
