c2b9d8cc2209625dcf5f666384af5fd399c421e1
ql605-at-547682380717 <quanglinh605@gmail.com>
2024-11-20 12:01:20 +0700

    init

--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,6 @@ import { type Composer } from "vue-i18n";
 
 const i18n: Composer = useI18n();
 const localeRoute = useLocaleRoute();
-
 useSeoMeta({
   title: i18n.t("seo_home_title"),
   ogTitle: i18n.t("seo_home_title"),
